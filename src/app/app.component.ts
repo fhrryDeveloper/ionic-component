e9e9ddee208311fd89f341aed2e98e00fcf764ac
fhrryDeveloper <fhrrydeveloper@gmail.com>
2021-04-25 06:04:20 +0300

    studing ion-menu button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private menu: MenuController) { }
+    openFirst() {
+      this.menu.enable(true, 'first');
+      this.menu.open('first');
+    }
+
+    openEnd() {
+      this.menu.open('end');
+    }
+
+    openCustom() {
+      this.menu.enable(true, 'custom');
+      this.menu.open('custom');
+    }
 }
